b166e8c6b8d57500d43279047a290b03b1e5399a
PatateHunter <45663157+PatateHunter@users.noreply.github.com>
2022-01-16 22:21:08 +0100

    test deploy ichai + ame 1

---
+++ server.js
@@ -0,0 +1,11 @@
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(express.static('dist/pokemon-arena'));
+app.get('/*', function(req,res) {res.sendFile(path.join(__dirname + 'dist/pokemon-arena'));});
+app.listen(process.env.PORT || 8080);
+
+
+
