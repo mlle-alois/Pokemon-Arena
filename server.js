0ee813ae317a68f4e0f573c721a27f7ed2b3bbb6
PatateHunter <45663157+PatateHunter@users.noreply.github.com>
2022-01-16 22:47:22 +0100

    ICH AME ALOIS 1

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-app.use(express.static('/dist/pokemon-arena'));
+app.use(express.static(__dirname+'/dist/pokemon-arena'));
 app.get('/*', function(req,res) {res.sendFile(path.join(__dirname + '/dist/pokemon-arena/index.html'));});
 app.listen(process.env.PORT || 8080);
 
