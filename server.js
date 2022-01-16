624991589b3db71060dabb1e650c80ade51b8849
PatateHunter <45663157+PatateHunter@users.noreply.github.com>
2022-01-16 22:51:41 +0100

    ICH AME ALOIS 1 KEBAB

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-app.use(express.static(__dirname+'/dist/pokemon-arena'));
-app.get('/*', function(req,res) {res.sendFile(path.join(__dirname + '/dist/pokemon-arena/index.html'));});
+app.use(express.static(__dirname+'/dist/Pokemon-Arena'));
+app.get('/*', function(req,res) {res.sendFile(path.join(__dirname + '/dist/Pokemon-Arena/index.html'));});
 app.listen(process.env.PORT || 8080);
 
 
