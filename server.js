dc6bce10b8e3fb7c5e0ee961e214e224145ab00f
PatateHunter <45663157+PatateHunter@users.noreply.github.com>
2022-01-16 22:41:18 +0100

    ICH AME 3

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const app = express();
 app.use(express.static('/dist/pokemon-arena'));
-app.get('/*', function(req,res) {res.sendFile(path.join(__dirname + '/dist/pokemon-arena'));});
+app.get('/*', function(req,res) {res.sendFile(path.join(__dirname + '/dist/pokemon-arena/index.html'));});
 app.listen(process.env.PORT || 8080);
 
 
